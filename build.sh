7e91af4166970f780d78942492f4172ec3cb2912
parmlabs01 <parmlabsltd@gmail.com>
2026-09-13 14:20:12 +0100

    Add build script for Flutter web project

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+git clone https://github.com/flutter/flutter.git -b stable --depth 1
+export PATH="$PATH:$(pwd)/flutter/bin"
+
+flutter doctor
+flutter config --enable-web
+flutter pub get
+flutter build web --release
